1b8b63d7fb2421b6b0fab4384124b5b7645447f2
Alex Berthon <alexberthon@dhcp-39-34-122.dhcpint.csulb.edu>
2016-09-07 13:44:48 -0700

    Initial Commit

---
+++ LabP3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LabP3
+//
+//  Created by Alex Berthon on 9/7/16.
+//  Copyright © 2016 Alex Berthon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
